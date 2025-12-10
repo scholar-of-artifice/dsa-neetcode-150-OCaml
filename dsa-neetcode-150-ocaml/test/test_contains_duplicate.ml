ad4795d589fa02039ddb36a801eff3906b57787a
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-10 23:57:49 +0000

    provide test cases

--- dsa-neetcode-150-ocaml/test/test_contains_duplicate.ml
+++ dsa-neetcode-150-ocaml/test/test_contains_duplicate.ml
@@ -2,9 +2,27 @@
 open Dsa_neetcode_150_ocaml
 
 let check_solution name solve_func = 
-    print_string ("GIVEN...");
-    print_string ("WHEN...");
-    print_string ("THEN...")
+    print_endline("Testing " ^ name ^ "...");
+    assert ( solve_func [] = false);
+    assert ( solve_func [   1   ] = false);
+    assert ( solve_func [   1; 2; 3; 4  ] = false);
+    assert ( solve_func [   1; 1; 3; 4  ] = true);
+    assert ( solve_func [   1; 2; 3; 1  ] = true);
+    assert ( solve_func [   0; 0; 0; 0; 0   ] = true);
+    assert ( solve_func [   1; 2; 3; 4; 5   ] = false);
+    assert ( solve_func [   1; 2; 3; 4; 5;
+                            1; 2; 3; 4; 5;
+                            1; 2; 3; 4; 5;
+                            1; 2; 3; 4; 5;
+                            1; 2; 3; 4; 5;
+                            1; 2; 3; 4; 5;  ] = true);
+    assert ( solve_func [   1; 2; 3; 4; 5;
+                            11; 12; 13; 14; 15;
+                            21; 22; 23; 24; 25;
+                            31; 32; 33; 34; 35;
+                            41; 42; 43; 44; 45;
+                            51; 52; 53; 54; 55;  ] = false);
+    print_endline "[OK] Passed"
     
 let run() = 
     print_endline "--- Contains Duplicate ---";
