c2d6ad3253f787ad941208e9740997383c8eb1e1
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-10 23:24:58 +0000

    create a test

---
+++ dsa-neetcode-150-ocaml/test/test_contains_duplicate.ml
@@ -0,0 +1,11 @@
+
+open Dsa_neetcode_150_ocaml
+
+let check_solution name solve_func = 
+    print_string ("GIVEN...");
+    print_string ("WHEN...");
+    print_string ("THEN...")
+    
+let run() = 
+    print_endline "--- Contains Duplicate ---";
+    check_solution "Sorting" Contains_duplicate.Sorting.solution
