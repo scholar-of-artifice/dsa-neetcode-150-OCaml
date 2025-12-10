babe816675f9c659bd4d9c7bc977f5293ff25405
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-10 23:58:32 +0000

    sorting based solution

--- dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
+++ dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
@@ -1,6 +1,14 @@
 
 module Sorting = struct
     let solution nums = 
-        (* todo *)
-        false
+        (* sort the list *)
+        let nums_sorted = List.sort compare nums in 
+        (* check adjacent elements *) 
+        let rec check_adjacent_elements = function
+            | [] -> false (* empty array *)
+            | [_] -> false (* single element *)
+            | x :: y  :: rest -> 
+                if x = y then true else check_adjacent_elements ( y :: rest)
+        in
+        check_adjacent_elements nums_sorted
 end
