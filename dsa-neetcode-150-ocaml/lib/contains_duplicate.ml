cea8c6cf291cc1aefb40a85d5c7d74cfb5b77fde
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-11 10:57:50 +0000

    add tests to end of file to use ppx_expect. use core instead of raw ocaml

--- dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
+++ dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
@@ -1,29 +1,36 @@
+open Core
 
 module Sorting = struct
     let solution nums = 
         (* sort the list *)
-        let nums_sorted = List.sort compare nums in 
+        let nums_sorted = List.sort nums ~compare:Int.compare in 
         (* check adjacent elements *) 
         let rec check_adjacent_elements = function
             | [] -> false (* empty array *)
             | [_] -> false (* single element *)
             | x :: y  :: rest -> 
-                if x = y then true else check_adjacent_elements ( y :: rest)
+                if Int.equal x y then true
+                else check_adjacent_elements ( y :: rest)
         in
         check_adjacent_elements nums_sorted
 end
 
 module Hashing = struct
-    module IntSet = Set.Make(Int)
 
     let solution nums = 
         (*use 'in' after local variable definitions*)
         (*start with an empty set*)
-        let set_observed = IntSet.of_list nums in
+        let set_observed = Set.of_list (module Int) nums in
 
         (* compare the size of each data structure *)
         (* .cardinal is how you get the size of a set *)
         (* <> is the not equals operation *)
-        List.length nums <> IntSet.cardinal set_observed
+        List.length nums <> Set.length set_observed
 end
 
+(*--- Tests ---*)
+
+let%expect_test "contains_duplicate -> sorting" =
+    let result = Sorting.solution [1; 2; 3; 1] in
+    Printf.printf "%b" result;
+    [%expect {|true|}]
