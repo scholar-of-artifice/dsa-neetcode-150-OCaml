ff258f842d4e3863a3d3abbbd86637111b12c8f3
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2025-12-11 09:10:28 +0000

    add the set/hashing solution for this problem

--- dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
+++ dsa-neetcode-150-ocaml/lib/contains_duplicate.ml
@@ -12,3 +12,18 @@ module Sorting = struct
         in
         check_adjacent_elements nums_sorted
 end
+
+module Hashing = struct
+    module IntSet = Set.Make(Int)
+
+    let solution nums = 
+        (*use 'in' after local variable definitions*)
+        (*start with an empty set*)
+        let set_observed = IntSet.of_list nums in
+
+        (* compare the size of each data structure *)
+        (* .cardinal is how you get the size of a set *)
+        (* <> is the not equals operation *)
+        List.length nums <> IntSet.cardinal set_observed
+end
+
